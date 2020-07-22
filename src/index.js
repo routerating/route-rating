4aa1df06b74a8853f28af26a295c0c0a67ae7110
Luke Shay <lshay@sourceallies.com>
2020-07-22 18:25:09 -0500

    added ci and hosting

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
+import { hydrate, render } from 'react-dom'
+
 import Amplify from 'aws-amplify'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import awsExports from './aws-exports'
 import { register as registerServiceWorker } from './serviceWorker'
 
 Amplify.configure(awsExports)
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-)
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement,
+  )
+} else {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement,
+  )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
